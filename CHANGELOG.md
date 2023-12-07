6860f943f6e5439460b1ec2c0b82134481739727
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-12-07 19:23:59 +0100

    support no_std

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,6 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+* `no_std` support (by disabling the `std` feature)
 
 [Unreleased]: https://github.com/jcornaz/cell-grid/compare/...HEAD
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+//! A simple cellular grid container suitable for `no_std` game development
+//!
+//! ## Features
+//!
+//! `std`: *(enabled by default)* enable use of the standard library. Must be disabled for `no_std` crates.
